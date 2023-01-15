62a6dd7b66245f6991066a1bbd2e28de5c2c71d2
ssekuwanda <cytixdoug@gmail.com>
2023-01-15 12:31:50 +0300

    added whitenoise

--- MoviesBack/settings.py
+++ MoviesBack/settings.py
@@ -23,6 +23,7 @@ else:
 # Application definitionss
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 ROOT_URLCONF = 'MoviesBack.urls'
 
 # Swagger settings
