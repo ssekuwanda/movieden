ebb859b14e832e50baef2ef4a71568f5df283441
ssekuwanda <cytixdoug@gmail.com>
2023-01-15 12:34:46 +0300

    editted urls

--- MoviesBack/urls.py
+++ MoviesBack/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
-+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
